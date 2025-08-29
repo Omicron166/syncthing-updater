8831e28b52eca83f7997dc755d783adf32820da6
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2025-08-29 12:18:45 +0200

    dead simple config generation script
    I'll improve it later. Maybe I should specify all the available platforms and architectures on the readme.
    
    Nah, no one reads it anyways.

---
+++ gen-config.py
@@ -0,0 +1,21 @@
+import json
+
+config = {
+    "bin_path": "/usr/bin/syncthing",
+    "upgrade_url": "https://upgrades.syncthing.net/meta.json",
+    "architecture": "syncthing-linux-amd64"
+}
+
+config["bin_path"] = input("Enter the path to the syncthing binary: ")
+
+upgrade_url = input("Enter the upgrade url (leave blank to use official syncthing update url): ")
+
+if upgrade_url:
+    config["upgrade_url"] = upgrade_url
+
+config["architecture"] = input("Enter device architecture (e.g. syncthing-linux-amd64): ")
+
+print("Writing generated configuration to config.json")
+
+with open("config.json", "w") as f:
+    json.dump(config, f)
